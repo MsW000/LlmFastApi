161002c02ccf4fe53094203f5aa6a2e8ecf606fe
Cernel_Friend <inakentius@gmail.com>
2026-06-26 11:10:53 +0300

    Changes ALL System Design

---
+++ app/memory_system/short_term/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, Text, ForeignKey
+from sqlalchemy.orm import relationship
+from pgvector.sqlalchemy import Vector
+from app.database import Base
+
+class Message(Base):
+    __tablename__ = "messages"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user_message = Column(Text, nullable=False)
+    ai_response = Column(Text, nullable=False)
+    embedding = Column(Vector(768))
+
+    # обратная связь
+    owner = relationship("User", back_populates="messages")
